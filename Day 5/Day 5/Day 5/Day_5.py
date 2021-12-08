51bdb061617e25219a87c265a5b65510b4f7f3b3
jere-dev <jeremthom1@gmail.com>
2021-12-07 23:43:53 -0500

    Day 5 Advent Of Code -Redemption (switched to python)

---
+++ Day 5/Day 5/Day 5/Day_5.py
@@ -0,0 +1,46 @@
+Part1 = {}
+Part2 = {}
+
+for line in open('../../Day-5-input.txt'):
+    first,last = line.split('->')
+    x1,y1 = first.split(',')
+    x2,y2 = last.split(',')
+    x1 = int(x1)
+    y1 = int(y1)
+    x2 = int(x2)
+    y2 = int(y2)
+
+    if x1==x2:
+        if y1>y2: 
+            y1,y2=y2,y1
+        for y in range(y1,y2+1):
+          Part1[(x1,y)]=Part1.get((x1,y),0)+1
+          Part2[(x1,y)]=Part2.get((x1,y),0)+1
+    elif y1==y2:
+        if x1>x2: 
+            x1,x2=x2,x1
+        for x in range(x1,x2+1):
+          Part1[(x,y1)]=Part1.get((x,y1),0)+1
+          Part2[(x,y1)]=Part2.get((x,y1),0)+1
+    else:
+        if x1>x2: 
+            x1,x2, y1,y2 = x2,x1, y2,y1
+        for x in range(x1,x2+1):
+          if y2>y1: 
+              y = y1+(x-x1)
+          else:     
+              y = y1-(x-x1)
+          Part2[(x,y)]=Part2.get((x,y),0)+1
+
+ans1 = 0
+for k in Part1:
+    if Part1[k] > 1:
+        ans1 += 1
+
+ans2 = 0
+for k in Part2:
+    if Part2[k] > 1:
+        ans2 += 1
+
+print("Part1: ",ans1)
+print("Part2: ",ans2)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Advent-of-code
 -🎄-This is my first Advent Of Code -🎄-  ⛄
+Day 1 - preaty easy⭐⭐
+Day 2 - easy⭐⭐
+Day 3 - did something with binary for the first time ⭐
+Day 4 - the squid won
+Day 5 - redmtion (switched to python)⭐⭐
